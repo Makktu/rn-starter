3815c5420c7254aee1b786c7fd9595e170e33ac6
Makktu <johnmcna@gmail.com>
2023-05-06 01:47:39 +0100

    yes

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const ColorCounter = ({ type }) => {
+const ColorCounter = ({ type, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text style={{ color: "orangered", fontSize: 20 }}>{type}</Text>
-      <Button title={`Increase ${type}`} />
-      <Button title={`Decrease ${type}`} />
+      <Button onPress={() => onIncrease()} title={`Increase ${type}`} />
+      <Button onPress={() => onDecrease()} title={`Decrease ${type}`} />
     </View>
   );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import MyView from "../components/MyView";
 import MyText from "../components/MyText";
 
 import ColorCounter from "../components/ColorCounter";
 
+let COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <MyView>
-      <ColorCounter type="Red" />
-      <ColorCounter type="Green" />
-      <ColorCounter type="Blue" />
+      <ColorCounter
+        onIncrease={() => (red < 255 ? setRed(red + COLOR_INCREMENT) : null)}
+        onDecrease={() => (red > 0 ? setRed(red - COLOR_INCREMENT) : null)}
+        type="Red"
+      />
+      <ColorCounter
+        onIncrease={() =>
+          green < 255 ? setGreen(green + COLOR_INCREMENT) : null
+        }
+        onDecrease={() =>
+          green > 0 ? setGreen(green - COLOR_INCREMENT) : null
+        }
+        type="Green"
+      />
+      <ColorCounter
+        onIncrease={() => (blue < 255 ? setBlue(blue + COLOR_INCREMENT) : null)}
+        onDecrease={() => (blue > 0 ? setBlue(blue - COLOR_INCREMENT) : null)}
+        type="Blue"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      />
     </MyView>
   );
 };
